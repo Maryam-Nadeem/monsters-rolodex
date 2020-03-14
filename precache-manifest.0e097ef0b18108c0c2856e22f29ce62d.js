5a88a7ea19d21f557c38c981b82112ab0d2af09b
Maryam Nadeem <maryamnadeem821@gmail.com>
2020-03-14 11:31:30 +0500

    Updates

---
+++ precache-manifest.0e097ef0b18108c0c2856e22f29ce62d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c598b8c066e686998d23290c4a4b7e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1376014f6954b6207ba1",
+    "url": "/monsters-rolodex/static/css/main.38b9e395.chunk.css"
+  },
+  {
+    "revision": "7fe0845a347327439809",
+    "url": "/monsters-rolodex/static/js/2.80027728.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.80027728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1376014f6954b6207ba1",
+    "url": "/monsters-rolodex/static/js/main.836c5fce.chunk.js"
+  },
+  {
+    "revision": "0a7538a84e8ef0422b17",
+    "url": "/monsters-rolodex/static/js/runtime-main.984b980e.js"
+  }
+]);
